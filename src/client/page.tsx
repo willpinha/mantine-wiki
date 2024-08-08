4174548827039e08ff33a5bfb424eca894098e61
Willian Pinheiro <willpinhaaa@gmail.com>
2024-08-07 21:43:15 -0300

    initialize mantine with astro project

---
+++ src/client/page.tsx
@@ -0,0 +1,30 @@
+import "@mantine/core/styles.css";
+
+import "@mantine/carousel/styles.css";
+import "@mantine/charts/styles.css";
+import "@mantine/code-highlight/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/nprogress/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/tiptap/styles.css";
+
+import { Navbar } from "@/components/navbar";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({});
+
+type ClientPageProps = {
+	children: React.ReactNode;
+};
+
+export function ClientPage({ children }: ClientPageProps) {
+	return (
+		<MantineProvider theme={theme}>
+			<Navbar />
+
+			{children}
+		</MantineProvider>
+	);
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,108 @@
+import {
+	ActionIcon,
+	AppShell,
+	Burger,
+	Divider,
+	Drawer,
+	Group,
+	Image,
+	Tooltip,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import {
+	IconBrandGithub,
+	IconBrandMantine,
+	IconCoffee,
+	IconPalette,
+	IconSearch,
+	IconTags,
+} from "@tabler/icons-react";
+
+export function Navbar() {
+	const [navbarOpened, { toggle: toggleNavbar }] = useDisclosure();
+	const [themesOpened, { toggle: toggleThemes, close: closeThemes }] =
+		useDisclosure();
+
+	return (
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{
+				width: 300,
+				breakpoint: "sm",
+				collapsed: { desktop: true, mobile: !navbarOpened },
+			}}
+			padding="md"
+		>
+			<Drawer
+				opened={themesOpened}
+				onClose={closeThemes}
+				title="Mantine themes"
+				position="right"
+				offset={15}
+				radius="md"
+				size="xs"
+			>
+				{/* Drawer content */}
+			</Drawer>
+
+			<AppShell.Header>
+				<Group h="100%" px="md">
+					<Burger
+						opened={navbarOpened}
+						onClick={toggleNavbar}
+						hiddenFrom="sm"
+						size="sm"
+					/>
+
+					<Group justify="space-between" style={{ flex: 1 }}>
+						<ActionIcon size="xl" component="a" href="/">
+							<Image src="/logo.svg" w={35} />
+						</ActionIcon>
+
+						<Group>
+							<Tooltip label="Tags">
+								<ActionIcon>
+									<IconTags />
+								</ActionIcon>
+							</Tooltip>
+
+							<Tooltip label="Mantine components">
+								<ActionIcon>
+									<IconBrandMantine />
+								</ActionIcon>
+							</Tooltip>
+
+							<Tooltip label="Search components">
+								<ActionIcon>
+									<IconSearch />
+								</ActionIcon>
+							</Tooltip>
+
+							<Tooltip label="Themes">
+								<ActionIcon onClick={toggleThemes}>
+									<IconPalette />
+								</ActionIcon>
+							</Tooltip>
+
+							<Divider orientation="vertical" />
+
+							<Tooltip label="Buy me a coffee">
+								<ActionIcon>
+									<IconCoffee />
+								</ActionIcon>
+							</Tooltip>
+
+							<Tooltip label="GitHub">
+								<ActionIcon>
+									<IconBrandGithub />
+								</ActionIcon>
+							</Tooltip>
+						</Group>
+					</Group>
+				</Group>
+			</AppShell.Header>
+
+			<AppShell.Navbar>Navbar</AppShell.Navbar>
+		</AppShell>
+	);
+}
